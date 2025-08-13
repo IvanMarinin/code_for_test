86b6ec783c0b322d63d73de783795670e8099b84
IvanMarinin <marinin.ivan.alexeevich@gmail.com>
2025-08-13 23:33:34 +0300

    Couple changes

--- spectrum_class.py
+++ spectrum_class.py
@@ -1,70 +1,97 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import pywt
-from scipy.signal import savgol_filter, butter, filtfilt
-from signal_class import Signal
 from scipy.stats import skew, kurtosis, entropy, gaussian_kde
-
+from signal_class import Signal
 
 
 class Spectrum(Signal):
-   def __init__(self, filename, wavelengths, intensities, h_u=None, h_g=None, h_e=None, h_p=None, h_s=None, h_m=None, h_i=None):
-      super().__init__(filename, wavelengths, intensities)
-      self.h_u = h_u
-      self.h_g = h_g
-      self.h_e = h_e
-      self.h_p = h_p
-      self.h_s = h_s
-      self.h_m = h_m
-      self.h_i = h_i
+    def __init__(self, filename, wavelengths, intensities,
+                 h_u=None, h_g=None, h_e=None, h_p=None,
+                 h_s=None, h_m=None, h_i=None):
+        super().__init__(filename, wavelengths, intensities)
+        self.h_u = h_u
+        self.h_g = h_g
+        self.h_e = h_e
+        self.h_p = h_p
+        self.h_s = h_s
+        self.h_m = h_m
+        self.h_i = h_i
 
-   @property
-   def mean(self):
-      return np.mean(self.intensities)
+    @property
+    def mean(self):
+        return np.mean(self.intensities)
 
-   @property
-   def std(self):
-       return np.std(self.intensities)
+    @property
+    def std(self):
+        return np.std(self.intensities)
 
-   @property
-   def Modality(self):
-       return 0
+    @property
+    def Modality(self):
+        return 0  # Надо дописать нормально
 
-   @property
-   def skewness(self):
-       return skew(self.intensities)
+    @property
+    def skewness(self):
+        return skew(self.intensities)
 
-   @property
-   def kurtosis(self):
-       return kurtosis(self.intensities)
+    @property
+    def kurtosis(self):
+        return kurtosis(self.intensities)
 
-   @property
-   def iqr(self):
-       return np.percentile(self.intensities, 75) - np.percentile(self.intensities, 25)
+    @property
+    def iqr(self):
+        return np.percentile(self.intensities, 75) - np.percentile(self.intensities, 25)
 
-   @property
-   def snr(self):
-       numerator = np.sqrt(np.sum(self.intensities ** 2) / len(self.intensities))
-       denominator = np.sqrt(np.sum((self.intensities - self.mean) ** 2) / (len(self.intensities) - 1))
+    @property
+    def snr(self):
+        numerator = np.sqrt(np.sum(self.intensities ** 2) / len(self.intensities))
+        denominator = np.sqrt(np.sum((self.intensities - self.mean) ** 2) / (len(self.intensities) - 1))
 
-       if denominator == 0:
-           raise ValueError("Denominator equals 0")
-       return numerator / denominator
+        if denominator == 0:
+            raise ValueError("Denominator equals 0")
+        return numerator / denominator
 
-   def max_i(self):
-       return np.max(self.intensities)
+    @property
+    def max_i(self):
+        return np.max(self.intensities)
 
-   @property
-   def sum_i(self):
-       return np.sum(self.intensities)
+    @property
+    def sum_i(self):
+        return np.sum(self.intensities)
 
-   @property
-   def Entropy(self):
-       hist, _ = np.histogram(self.intensities, bins=512, density=True)
-       hist = hist[hist > 0]
-       return entropy(hist)
+    @property
+    def entropy(self):
+        hist, _ = np.histogram(self.intensities, bins=512, density=True)
+        hist = hist[hist > 0]
+        return entropy(hist)  # Уточнить формулу
 
-   @property
-   def freq_magnitude(self):
-       freq_spectrum = np.fft.fft(self.intensities)
-       return np.abs(freq_spectrum).max()
+    @property
+    def freq_magnitude(self):
+        freq_spectrum = np.fft.fft(self.intensities)
+        return np.abs(freq_spectrum).max()
+
+    @property
+    def get_features_list(self):
+        return [
+            self.mean,
+            self.std,
+            self.Modality,
+            self.skewness,
+            self.kurtosis,
+            self.iqr,
+            self.snr,
+            self.max_i,
+            self.sum_i,
+            self.entropy,
+            self.freq_magnitude
+        ]
+
+    @property
+    def get_defect_params_list(self):
+        return [
+            self.h_i,
+            self.h_u,
+            self.h_g,
+            self.h_e,
+            self.h_p,
+            self.h_s,
+            self.h_m
+        ]
--- temp.py
+++ temp.py
@@ -1,10 +1,48 @@
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import struct
 from signal_class import Signal
 from read_data import load_data_from_excel_with_defect
 
 
-file_path = r"C:\Users\marin\OneDrive\Рабочий стол\Научка\Анализ спектра\Пробная задача про корреляции\Data_2Channel.xlsx"
-spectra = load_data_from_excel_with_defect(file_path)
+file_path2 = r"C:\Users\marin\OneDrive\Рабочий стол\Научка\Анализ спектра\Пробная задача про корреляции\Data_2Channel.xlsx"
+file_path3 = r"C:\Users\marin\OneDrive\Рабочий стол\Научка\Анализ спектра\Пробная задача про корреляции\Data_3channel.xlsx"
 
-print(spectra[2].mean)
+spectra2 = load_data_from_excel_with_defect(file_path2)
+spectra3 = load_data_from_excel_with_defect(file_path3)
+
+def correlation(spectra, draw=False):
+    features_data = [s.get_features_list for s in spectra]
+    params_data = [s.get_defect_params_list for s in spectra]
+
+    feature_columns = ["mean", "std", "modality", "skewness", "kurtosis", "iqr", "snr", "max_i", "sum_i", "entropy", "freq_magnitude"]
+    param_columns = ["h_i", "h_u", "h_g", "h_e", "h_p", "h_s", "h_m"]
+
+    df_features = pd.DataFrame(features_data, columns=feature_columns)
+    df_params = pd.DataFrame(params_data, columns=param_columns)
+    df = pd.concat([df_features, df_params], axis=1)
+    corr_matrix = df.corr(method='pearson')
+
+    if draw:
+        corr_subset = corr_matrix.loc[feature_columns, param_columns]
+
+        plt.figure(figsize=(10, 6))
+        sns.heatmap(
+            corr_subset,
+            annot=True,  # отображать значения коэффициентов на клетках
+            cmap="coolwarm",  # красно-синяя цветовая схема
+            center=0,  # белый цвет для нуля
+            linewidths=0.5
+        )
+        plt.title("Корреляция признаков спектров и параметров дефектов")
+        plt.xlabel("Параметры дефектов")
+        plt.ylabel("Признаки спектров")
+        plt.show()
+
+    return corr_matrix.loc[feature_columns, param_columns]
+
+
+correlation(spectra2, True)
+correlation(spectra3, True)
